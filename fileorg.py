8b4fd846832c36bda75b0b01c27e612e513ef973
Mecuriouseus <kozlovm609@gmail.com>
2026-04-10 20:18:03 +0300

    edit: refactor, doesn't operate with folders without suitable file

--- fileorg.py
+++ fileorg.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 root_folder = "/mnt/nv2/spacebox/source/python-source/fileorg/test/h"
+# root_folder = "C:\\Users\\aksel\\OneDrive\\Рабочий стол\\123"
 
 if os.path.exists(root_folder) == False:
     print("Folder", root_folder, "Does not exist!")
@@ -10,38 +11,35 @@ if os.path.exists(root_folder) == False:
 
 
 root_content = os.listdir(root_folder)
+root_dirs = []
+root_files = []
 
-for dir in root_content:
-    dir_fullpath = os.path.join(root_folder, dir)
-    if os.path.isdir(dir_fullpath):
-        pdf_sub_dir = os.path.join(dir_fullpath, "pdf")
-        kra_sub_dir = os.path.join(dir_fullpath, "kra")
-        png_sub_folder = os.path.join(dir_fullpath, "png")
+for item in root_content:
+    item_fullpath = os.path.join(root_folder, item)
+    if os.path.isdir(item_fullpath):
+        root_dirs.append(item)
+    elif os.path.isfile(item_fullpath):
+        root_files.append(item)
 
-        os.makedirs(pdf_sub_dir, exist_ok=True)
-        os.makedirs(kra_sub_dir, exist_ok=True)
-        os.makedirs(png_sub_folder, exist_ok=True)
-
-        for file in root_content:
-            if "_" + dir in file:
-                if "pdf" in file:
-                    try:
-                        shutil.move(os.path.join(root_folder, file), pdf_sub_dir)
-                    except Exception as err:
-                        print(err)
-                elif "png" in file:
-                    try:
-                        shutil.move(os.path.join(root_folder, file), png_sub_folder)
-                    except Exception as err:
-                        print(err)
-                elif "kra" in file:
-                    try:
-                        shutil.move(os.path.join(root_folder, file), kra_sub_dir)
-                    except Exception as err:
-                        print(err)
+for file in root_files:
+    for dir in root_dirs:
+        if dir in file and "_" + dir in file:
+            dir_fullpath = os.path.join(root_folder, dir)
+            if "png" in file:
+                try:
+                    png_sub_folder = os.path.join(dir_fullpath, "png")
+                    os.makedirs(png_sub_folder, exist_ok=True)
+                    shutil.move(os.path.join(root_folder, file), png_sub_folder)
+                except Exception as err:
+                    print(err)
+            elif "kra" in file:
+                try:
+                    kra_sub_dir = os.path.join(dir_fullpath, "kra")
+                    os.makedirs(kra_sub_dir, exist_ok=True)
+                    shutil.move(os.path.join(root_folder, file), kra_sub_dir)
+                except Exception as err:
+                    print(err)
             else:
                 continue
-    else:
-        continue
 
 input("Press Enter to exit...")
